8eb8ec8083e97931a994172a5de6cf3c1668352b
Igor Mironov <mcs6502-sek@yahoo.com.au>
2014-08-11 00:18:13 +1000

    check that encodings retain the new method; add the empty string test

--- test/regexp_m17n_test.rb
+++ test/regexp_m17n_test.rb
@@ -27,8 +27,21 @@ class RegexpTest < MiniTest::Unit::TestCase
     Encoding.list.each do |enc|
       str = encode('.', enc)
       if str
+        # non-dummy encodings should have been primed during the first pass
+        assert(enc.dummy? || enc.respond_to?(:non_empty_str?))
+
         assert(RegexpM17N.non_empty?(str))
         assert(RegexpM17N.non_empty?(str))
+      end
+    end
+  end
+
+  def test_empty_string_using_all_code_converters_we_could_find
+    Encoding.list.each do |enc|
+      str = encode('', enc)
+      if str
+        assert(!RegexpM17N.non_empty?(str))
+        assert(!RegexpM17N.non_empty?(str))
       end
     end
   end
